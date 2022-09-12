d77e1c1a8a0e1de71338fa027801a267d9c7628f
bamm2 <96399409+bamm2@users.noreply.github.com>
2022-09-13 08:54:18 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 900 ms, Memory: 237244 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for (int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int ans =a+b;
+			bw.write(ans+ "\n"); // Writer는 반환값을 string으로 반환해야되므로 문자열 결합 
+		}
+		bw.flush();
+		br.close();
+		bw.close();
+
+	}
+
+}
