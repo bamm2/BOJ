f2a8e68562a244a6f22df8ea54106d020a989163
bamm2 <96399409+bamm2@users.noreply.github.com>
2022-09-25 08:38:01 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11744 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -5,50 +5,42 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int[][] map;
-    static int V,E,ans;
+    static int V, E, ans;
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-         V=Integer.parseInt(br.readLine());
-         E=Integer.parseInt(br.readLine());
+        V = Integer.parseInt(br.readLine());
+        E = Integer.parseInt(br.readLine());
 
-        map=new int[V+1][V+1];
-        visited=new boolean[V+1];
+        map = new int[V + 1][V + 1];
+        visited = new boolean[V + 1];
 
-        for(int i=0;i<E;i++){
-                st=new StringTokenizer(br.readLine()," ");
-                int a=Integer.parseInt(st.nextToken());
-                int b=Integer.parseInt(st.nextToken());
-                map[a][b]=map[b][a]=1;
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            map[a][b] = map[b][a] = 1;
         }
 
-        bfs(1);
-        bw.write(ans+"\n");
+        dfs(1);
+        bw.write(ans + "\n");
         bw.flush();
         bw.close();
         br.close();
     }
 
-    private static void bfs(int start){
-        Queue<Integer> q =new ArrayDeque<>();
-       visited[start]=true;
-       q.offer(start);
+    private static void dfs(int start) {
+        visited[start]=true;
 
-        while(!q.isEmpty()){
-            start =q.poll();
-            for(int i=1; i<V+1;i++){
-                if(map[start][i]==1 && !visited[i]) {
-                    q.offer(i);
-                    ans++;
-                    visited[i] = true;
-                }
+        for(int i=1;i<=V;i++){
+            if(map[start][i]==1 && !visited[i]) {
+                ans++;
+                dfs(i);
             }
-
         }
     }
-
 }
