b01fd2f574410a8123f8b584c6c6b1b3a658c374
bamm2 <96399409+bamm2@users.noreply.github.com>
2022-11-03 20:41:03 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 96 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.java
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.java
@@ -1,58 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N,arr[],min,max,sign[];
-    static boolean[] visited;
+    static int N,numbers[],sign[],max,min;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N=Integer.parseInt(br.readLine());
+        numbers=new int[N];
+
         st=new StringTokenizer(br.readLine()," ");
-        arr=new int[N];
-        sign=new int[4];
         for(int i=0;i<N;i++){
-            arr[i]=Integer.parseInt(st.nextToken());
+            numbers[i]=Integer.parseInt(st.nextToken());
         }
+
+        sign=new int[4];
         st=new StringTokenizer(br.readLine()," ");
         for(int i=0;i<4;i++){
-            sign[i]=Integer.parseInt(st.nextToken());  // [0]+ [1]- [2]* [3]/
+            sign[i]=Integer.parseInt(st.nextToken());
         }
-        min=Integer.MAX_VALUE;
         max=Integer.MIN_VALUE;
-        solve(1,arr[0]);
+        min=Integer.MAX_VALUE;
+        solve(1,numbers[0]);
         System.out.println(max);
         System.out.println(min);
+
     }
     private static void solve(int idx,int sum){
         if(idx==N){
-            min=min<sum?min:sum;
-            max=max>sum?max:sum;
+            max=Math.max(max,sum);
+            min=Math.min(min,sum);
             return;
         }
-        for(int d=0;d<4;d++){
-            if(sign[d]==0) continue;
-                sign[d]--;
-                switch (d){
-                    case 0:
-                        solve(idx+1,sum+arr[idx]);
-                        break;
-                    case 1:
-                        solve(idx+1,sum-arr[idx]);
-                        break;
-                    case 2:
-                        solve(idx+1,sum*arr[idx]);
-                        break;
-                    case 3:
-                        solve(idx+1,sum/arr[idx]);
-                        break;
-                }
-                sign[d]++;
+
+        for(int i=0;i<4;i++){
+            if(sign[i]==0) continue;
+            sign[i]--;
+            switch (i){
+                case 0:
+                    solve(idx+1,sum+numbers[idx]);
+                    break;
+                case 1:
+                    solve(idx+1,sum-numbers[idx]);
+                    break;
+                case 2:
+                    solve(idx+1,sum*numbers[idx]);
+                    break;
+                case 3:
+                    solve(idx+1,sum/numbers[idx]);
+                    break;
             }
+            sign[i]++;
         }
+
+    }
 }
