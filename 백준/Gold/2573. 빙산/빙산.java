8b70415c66931db7661e3dacf641be3e68bf8db9
bamm2 <96399409+bamm2@users.noreply.github.com>
2022-11-12 14:21:37 +0900

    [Gold IV] Title: 빙산, Time: 516 ms, Memory: 98240 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -6,116 +6,103 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Point {
-        int r, c;
 
-        Point(int r, int c) {
-            this.r = r;
-            this.c = c;
+    static int R,C,map[][];
+    static int[][] dir={ {-1,0}, {1,0}, {0,1}, {0,-1}};
+    static boolean[][] visited;
+    static class Point {
+        int r,c;
+        Point(int r,int c){
+            this.r=r;
+            this.c=c;
         }
     }
-
-    static int N, M, map[][],ans;
-    static boolean visited[][];
-    static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st;
 
-        N = Integer.parseInt(st.nextToken()); // 행
-        M = Integer.parseInt(st.nextToken()); // 열
+        st=new StringTokenizer(br.readLine()," ");
+        R=Integer.parseInt(st.nextToken());
+        C=Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        map=new int[R][C];
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+        for(int i=0;i<R;i++){
+            st=new StringTokenizer(br.readLine()," ");
+            for(int j=0;j<C;j++){
+                map[i][j]=Integer.parseInt(st.nextToken());
             }
         }
-        ans=0;
 
+        int ans=0;
         while(true) {
-            visited= new boolean[N][M];
-
-            int chk = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
+            int cnt = 0;
+            visited = new boolean[R][C];
+            for (int i = 0; i < R; i++) {
+                for (int j = 0; j < C; j++) {
                     if (map[i][j] != 0 && !visited[i][j]) {
                         bfs(i, j);
-                        chk++;
+                        cnt++;
                     }
                 }
             }
-            if(chk==0) { // 나눠지지 않고 다 녹아버릴 경우
-                ans=0;
-                break;
-            }else if(chk>=2) { // 나눠진 경우
+
+            if (cnt >= 2) break;
+            if (cnt == 0) {
+                ans = 0;
                 break;
             }
-            
+
             melt();
             ans++;
-
         }
-
         System.out.println(ans);
-
-//        for(int[] a : map){
-//            for(int b : a){
-//                System.out.print(b+" ");
-//            }
-//            System.out.println();
-//        }
     }
-    private static void melt(){
-        visited=new boolean[N][M];
 
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                if(map[i][j]!=0){
-                    if(map[i][j]!=0)  visited[i][j]=true;
+    private static void melt(){
+        visited=new boolean[R][C];
+
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                if(map[i][j]!=0) {
+                    visited[i][j]=true;
                     for(int d=0;d<4;d++){
-                        int cnt=0;
                         int nr=i+dir[d][0];
                         int nc=j+dir[d][1];
 
                         if(isOut(nr,nc)) continue;
+                        if(visited[nr][nc]) continue;
+                        int cnt=0;
                         if(!visited[nr][nc] && map[nr][nc]==0) cnt++;
-                        if(map[i][j]>0){
-                            if(map[i][j]-cnt<0) map[i][j]=0;
-                            else map[i][j]-=cnt;
-                        }
+                        if(map[i][j]-cnt>0) map[i][j]-=cnt;
+                        else map[i][j]=0;
                     }
                 }
             }
         }
     }
-    private static void bfs(int r ,int c ) {
-        Queue<Point> q = new ArrayDeque<>();
-        q.offer(new Point(r, c));
-        visited[r][c] = true;
 
+    private static void bfs(int r, int c){
+        Queue<Point> q =new ArrayDeque<>();
+        q.offer(new Point(r,c));
+        visited[r][c]=true;
 
-        while (!q.isEmpty()) {
-            Point curr = q.poll();
-            for (int d = 0; d < 4; d++) {
+        while(!q.isEmpty()){
+            Point curr=q.poll();
+            for(int d=0;d<4;d++) {
                 int nr = curr.r + dir[d][0];
                 int nc = curr.c + dir[d][1];
-
                 if (isOut(nr, nc)) continue;
-                if (!visited[nr][nc] && map[nr][nc] != 0) {
-                    q.offer(new Point(nr, nc));
-                    visited[nr][nc] = true;
+                if (map[nr][nc]!=0 && !visited[nr][nc]){
+                    visited[nr][nc]=true;
+                    q.offer(new Point(nr,nc));
                 }
             }
         }
     }
 
-    private static boolean isOut(int r, int c) {
-        if (r < 0 || c < 0 || r >= N || c >= M) return true;
-        return false;
+    private static boolean isOut(int r, int c){
+        return (r<0 || c<0 || r>=R || c>=C );
     }
-
 }
