03b0ddf2f33045712294ce62f35e11caee7ba940
bamm2 <96399409+bamm2@users.noreply.github.com>
2022-12-14 16:53:54 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 488 ms, Memory: 118860 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,46 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N,M,map[][];
+    static int V,E,map[][];
     static boolean[] visited;
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N=Integer.parseInt(st.nextToken()); // 정점의 개수
-        M=Integer.parseInt(st.nextToken()); // 간선의 개수
-        map=new int[N+1][N+1];
-        for(int i=0;i<M;i++){
+        StringTokenizer st;
+
+        st=new StringTokenizer(br.readLine()," ");
+        V=Integer.parseInt(st.nextToken());
+        E=Integer.parseInt(st.nextToken());
+
+        map=new int[V+1][V+1];
+        visited=new boolean[V+1];
+
+        for(int i=0;i<E;i++){
             st=new StringTokenizer(br.readLine()," ");
-            int start =Integer.parseInt(st.nextToken());
-            int ed =Integer.parseInt(st.nextToken());
-            map[start][ed]=map[ed][start]=1;
+            int from=Integer.parseInt(st.nextToken());
+            int to=Integer.parseInt(st.nextToken());
+            map[from][to]=map[to][from]=1;
         }
-        visited=new boolean[N+1];
 
         int ans=0;
-        for(int i=1;i<=N;i++){
-            if(!visited[i]){
-               dfs(i);
-                ans++;
-            }
+        for(int i=1;i<=V;i++){
+                if(!visited[i]){
+                    ans++;
+                    solve(i);
+                }
         }
 
         System.out.println(ans);
-
+        br.close();
     }
-    private static void dfs(int idx){
 
-        visited[idx]= true;
 
-        for(int i=1;i<=N;i++){
-            if(!visited[i] && map[idx][i]==1){
-                dfs(i);
+    private static void solve(int r){
+        Queue<Integer> q= new ArrayDeque<>();
+        q.offer(r);
+
+        while(!q.isEmpty()){
+            Integer curr =q.poll();
+            for(int i=1;i<=V;i++){
+                if(map[curr][i]!=0 && !visited[i]){
+                    q.offer(i);
+                    visited[i]=true;
+                }
             }
         }
     }
+
 }
