cd3116623b7e77681e42c36fcdf605c4ec1cae53
bamm2 <96399409+bamm2@users.noreply.github.com>
2023-01-23 11:01:42 +0900

    [Bronze V] Title: 개수 세기, Time: 76 ms, Memory: 11564 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+       int N=Integer.parseInt(br.readLine());
+
+       int[] arr=new int[201];
+        StringTokenizer st =new StringTokenizer(br.readLine()," ");
+       for(int i=0;i<N;i++){
+           arr[Integer.parseInt(st.nextToken())+100]++;
+       }
+
+       int M=Integer.parseInt(br.readLine());
+
+        System.out.println(arr[M+100]);
+
+
+    }
+}
