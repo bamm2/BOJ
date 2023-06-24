fca4a082d6ce4e6c276c99b3d693783d23b61573
bamm2 <96399409+bamm2@users.noreply.github.com>
2023-06-24 22:09:21 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -4,19 +4,20 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    static int N, arr[], dp[], ans;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int num= Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        int[] dp =new int[num+2];
-        dp[0]=0;
+        int[] dp =new int[1001];
         dp[1]=1;
         dp[2]=3;
-        for(int i=3;i<=num;i++){
-            dp[i]=(dp[i-2]*2 +dp[i-1])%10007;
+        for(int i=3;i<=N;i++){
+            dp[i]= (dp[i-1]+ 2*dp[i-2]) % 10007;
         }
-        System.out.println(dp[num]);
-        br.close();
-    } // main
+
+        System.out.println(dp[N]);
+    }
 }
