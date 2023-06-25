ad217686c1e0c4e8c8c1a0676b0eaab746e64983
bamm2 <96399409+bamm2@users.noreply.github.com>
2023-06-25 22:43:22 +0900

    [Silver III] Title: 계단 오르기, Time: 80 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,32 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        int N=Integer.parseInt(br.readLine());
-        int[] dp =new int[N+1];
-        int[] arr= new int[N+1];
-        for(int i=1;i<=N;i++){
-            arr[i]=Integer.parseInt(br.readLine());
-            }
+        int N = Integer.parseInt(br.readLine());
 
-        dp[1]=arr[1];
-        if(N>=2) {
-            dp[2] = arr[1] + arr[2];
-        if (N >= 3) {
-                dp[3] = Math.max(arr[1], arr[2]) + arr[3];
-                // 4번째까지 갈 수 있는 경우 1칸가고 2칸,1칸  or 2칸 ,2칸  ( 2칸,1칸,1칸은 연속이므로 )
-                for (int i = 4; i <= N; i++) {
-                    dp[i] = Math.max(dp[i - 3] + arr[i - 1], dp[i - 2]) + arr[i];
-                }
-            }
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        System.out.println(dp[N]);
-    } // main
+
+        int ans = 0 ;
+
+        if(N==1) ans = arr[0];
+        else if(N==2) ans =arr[0]+arr[1];
+        else{
+            dp[0]=arr[0];
+            dp[1]=arr[0]+arr[1];
+            dp[2]=Math.max(arr[0],arr[1])+arr[2];
+            for(int i=3;i<N;i++){
+                dp[i]=Math.max(dp[i-3]+arr[i-1],dp[i-2])+arr[i];
+            }
+            ans=dp[N-1];
+        }
+
+        System.out.println(ans);
+    }
 }
