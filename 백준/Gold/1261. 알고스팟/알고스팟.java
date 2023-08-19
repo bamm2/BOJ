dbedd43d54fb34957942d0a8338e113455190abf
bamm2 <96399409+bamm2@users.noreply.github.com>
2023-08-20 00:05:59 +0900

    [Gold IV] Title: 알고스팟, Time: 100 ms, Memory: 12132 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -2,19 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static final int INF = 10001;
-    static int R,C,map[][],dist[][];
+    static int ans ;
+    static int R,C,map[][];
+    static boolean[][] visited;
     static int[][] dir = { {-1,0} ,{1,0} ,{0,1},{0,-1}};
     static class Point{
-        int r,c;
-        Point(int r,int c){
+        int r,c,cnt;
+        Point(int r,int c,int cnt){
             this.r=r;
             this.c=c;
+            this.cnt=cnt;
         }
     }
 
@@ -25,36 +27,37 @@ public class Main {
         C=Integer.parseInt(st.nextToken());
         R=Integer.parseInt(st.nextToken());
         map=new int[R][C];
-        dist=new int[R][C];
+        visited=new boolean[R][C];
 
         for(int i=0;i<R;i++){
             String s = br.readLine();
             for(int j=0;j<C;j++){
                 map[i][j]=s.charAt(j)-'0';
-                dist[i][j]=INF;
             }
         }
 
-        dijkstra();
+        bfs();
 
-        System.out.println(dist[R-1][C-1]);
+        System.out.println(ans);
     }
 
-    private static void dijkstra(){
-        Queue<Point> q =new ArrayDeque<>();
-        q.offer(new Point(0,0));
-        dist[0][0]=0;
+    private static void bfs(){
+        Deque<Point> q =new ArrayDeque<>();
+        q.offer(new Point(0,0,0));
 
         while(!q.isEmpty()){
             Point curr = q.poll();
+            if(curr.r==R-1 && curr.c==C-1 ){
+                ans=curr.cnt;
+                break;
+            }
             for(int d=0;d<4;d++){
                 int nr= curr.r+dir[d][0];
                 int nc = curr.c+dir[d][1];
-                if(isOut(nr,nc)) continue;
-                if(dist[nr][nc]>dist[curr.r][curr.c]+map[nr][nc]){
-                    dist[nr][nc]=dist[curr.r][curr.c]+map[nr][nc];
-                    q.offer(new Point(nr,nc));
-                }
+                if(isOut(nr,nc) || visited[nr][nc]) continue;
+                visited[nr][nc]=true;
+                if(map[nr][nc]==1) q.offerLast(new Point(nr,nc, curr.cnt+1));
+                else q.offerFirst(new Point(nr,nc,curr.cnt));
             }
         }
 
