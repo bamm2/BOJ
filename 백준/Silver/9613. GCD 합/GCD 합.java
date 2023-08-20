3ebb58b0b8b63eee0bb7e5741d82a46b556f9de6
bamm2 <96399409+bamm2@users.noreply.github.com>
2023-08-20 13:55:02 +0900

    [Silver IV] Title: GCD 합, Time: 80 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb =new StringBuilder();
+        StringTokenizer st ;
+
+        int T =Integer.parseInt(br.readLine());
+
+        while(T-->0){
+            st=new StringTokenizer(br.readLine(), " ");
+            int  N =Integer.parseInt(st.nextToken());
+
+            int[] arr =new int[N];
+
+            for(int i=0;i<N;i++){
+                arr[i]=Integer.parseInt(st.nextToken());
+            }
+
+            Arrays.sort(arr);
+
+            long  sum =0 ;
+            for(int i=0;i<N-1;i++){
+                for(int j=i+1;j<N;j++){
+                   sum+=GCD(arr[j],arr[i]);
+                }
+            }
+
+            sb.append(sum).append('\n');
+        }
+
+        System.out.println(sb);
+
+    }
+
+    private static int GCD(int a ,int b){
+        if(a%b==0) {
+            return b;
+        }
+
+        return GCD(b,a%b);
+    }
+
+}
