a46e28fe36bbac0076c406e85a9dc5152a22ff1e
bamm2 <96399409+bamm2@users.noreply.github.com>
2023-10-04 22:41:01 +0900

    [Silver III] Title: 수열, Time: 192 ms, Memory: 22256 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,32 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
         st=new StringTokenizer(br.readLine()," ");
-        int N=Integer.parseInt(st.nextToken());
-        int K=Integer.parseInt(st.nextToken());
+        int N =Integer.parseInt(st.nextToken());
+        int K =Integer.parseInt(st.nextToken());
 
-        int[] arr= new int[N];
+        int[] arr =new int[N+1];
         st=new StringTokenizer(br.readLine()," ");
-        for(int i=0;i<N;i++){
+        for(int i=1;i<=N;i++){
             arr[i]=Integer.parseInt(st.nextToken());
-        }
-        int max=Integer.MIN_VALUE;
-        for(int i=0;i<=N-K;i++){
-            int sum=0;
-            for(int j=0;j<K;j++){
-                sum+=arr[i+j];
-            }
-            max=max>sum?max:sum;
+            arr[i]+=arr[i-1];
         }
 
-        System.out.println(max);
-    } // main
+        int ans = Integer.MIN_VALUE ;
+        int[] tmp =new int[N+1];
+        for(int i=K;i<=N;i++){
+            int sum = arr[i] - arr[i - K];
+            tmp[i]=sum;
+            if(sum > ans){
+                ans = sum;
+            }
+        }
+        System.out.println(ans);
+    }
 }
