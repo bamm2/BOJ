43691e1342d39c59bb9bafc9c637c98d551f0dac
bamm2 <96399409+bamm2@users.noreply.github.com>
2023-10-16 16:10:34 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 980 ms, Memory: 93088 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int V,E,start,depth;
+    static List<Integer>[] list;
+    static boolean[] visited;
+    static int[] ans;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb =new StringBuilder();
+        StringTokenizer st ;
+
+        st=new StringTokenizer(br.readLine()," ");
+        V=Integer.parseInt(st.nextToken());
+        E=Integer.parseInt(st.nextToken());
+        start=Integer.parseInt(st.nextToken());
+
+        list=new ArrayList[V+1];
+        visited=new boolean[V+1];
+        ans=new int[V+1];
+
+        for(int i=1;i<=V;i++){
+            list[i]=new ArrayList<>();
+        }
+
+        for(int i=0;i<E;i++){
+            st=new StringTokenizer(br.readLine()," ");
+            int from= Integer.parseInt(st.nextToken());
+            int to =Integer.parseInt(st.nextToken());
+            list[from].add(to);
+            list[to].add(from);
+        }
+
+        for(int i=1;i<=V;i++){
+            Collections.sort(list[i],Collections.reverseOrder());
+        }
+
+        dfs(start);
+
+        for(int i=1;i<=V;i++){
+           sb.append(ans[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int v) {
+        ans[v]=++depth;
+        visited[v]=true;
+
+        for(Integer next : list[v]){
+            if(visited[next]) continue;
+            dfs(next);
+        }
+
+    }
+}
