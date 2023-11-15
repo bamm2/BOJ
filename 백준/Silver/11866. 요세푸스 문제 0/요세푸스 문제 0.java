e2b86cfd245aff4e3a707c4cd50c30cf6aca5340
bamm2 <96399409+bamm2@users.noreply.github.com>
2023-11-15 16:11:34 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 100 ms, Memory: 12184 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,44 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine(), " ");
 
-		Queue<Integer> q=new LinkedList<>();
-		
-		for(int i=1;i<=N;i++) {
-			q.add(i);
-		}
-		
-		int j=0;
-		int[] arr =new int[N];
-		while(!q.isEmpty()) {
-			for(int i=0;i<K-1;i++) {
-			q.add(q.poll());
-				}
-			arr[j]=q.poll();
-			j++;
-		}
-		System.out.print("<");
-		for(int i=0;i<N;i++) {
-			if(i==N-1) {
-				System.out.print(arr[i]);
-			}else
-				System.out.print(arr[i]+", ");
-		}
-		System.out.println(">");
-			
-		
-		
-	}// main
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> q =new ArrayDeque<>();
+        for(int i=1;i<=N;i++){
+            q.offer(i);
+        }
+
+        sb.append("<");
+        while (q.size()!=1){
+            int count = K;
+            while (count-->1){
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+        sb.append(q.poll()).append(">");
+
+        System.out.println(sb);
+
+    }
 }
