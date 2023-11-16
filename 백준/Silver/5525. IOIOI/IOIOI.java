f0812c93e4f4735849b1e26c24b68a0f1ed91497
bamm2 <96399409+bamm2@users.noreply.github.com>
2023-11-16 23:12:38 +0900

    [Silver I] Title: IOIOI, Time: 276 ms, Memory: 30116 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static int[] table;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -11,14 +13,47 @@ public class Main {
         int M = Integer.parseInt(br.readLine());
         String s = br.readLine();
 
-        String findStr = getFindStr(N);
+        String pattern = getFindStr(N);
+        makeTable(pattern);
+
         int count = 0;
 
+        int idx = 0;
         for (int i = 0; i < M; i++) {
-            if (s.startsWith(findStr, i)) count++;
+
+            while (idx > 0 && s.charAt(i)!=pattern.charAt(idx)) {
+                idx = table[idx - 1];
+            }
+
+            if (s.charAt(i)==pattern.charAt(idx)) {
+                if (idx==pattern.length() - 1) {
+                    idx = table[idx];
+                    count++;
+                } else {
+                    idx++;
+                }
+            }
         }
 
         System.out.println(count);
+
+    }
+
+    private static void makeTable(String pattern) {
+        table = new int[pattern.length()];
+
+        int idx = 0;
+        for (int i = 1; i < table.length; i++) {
+
+            while (idx > 0 && pattern.charAt(idx)!=pattern.charAt(i)) {
+                idx = table[idx - 1];
+            }
+
+            if (pattern.charAt(idx)==pattern.charAt(i)) {
+                idx += 1;
+                table[i] = idx;
+            }
+        }
     }
 
     private static String getFindStr(int n) {
