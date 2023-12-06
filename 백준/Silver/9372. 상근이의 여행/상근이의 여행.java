180e6e9ae3e897609cf38647abab6aacd8197f6d
bamm2 <96399409+bamm2@users.noreply.github.com>
2023-12-07 07:37:38 +0900

    [Silver IV] Title: 상근이의 여행, Time: 304 ms, Memory: 43936 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -4,23 +4,21 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int V, E;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
             st = new StringTokenizer(br.readLine(), " ");
-            V = Integer.parseInt(st.nextToken());
-            E = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
             for (int i = 0; i < E; i++) {
                 st = new StringTokenizer(br.readLine(), " ");
-                int from = Integer.parseInt(st.nextToken());
-                int to = Integer.parseInt(st.nextToken());
+                Integer.parseInt(st.nextToken());
+                Integer.parseInt(st.nextToken());
             }
 
             sb.append(V - 1).append('\n');
