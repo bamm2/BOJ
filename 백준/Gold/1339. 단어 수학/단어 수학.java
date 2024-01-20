2a77fdbd6320e69dfc60b4a4d5bbef40ac9e0b59
bamm2 <96399409+bamm2@users.noreply.github.com>
2024-01-20 16:17:34 +0900

    [Gold IV] Title: 단어 수학, Time: 228 ms, Memory: 18408 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -4,69 +4,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     static int N, ans;
     static String[] arr;
-    static List<Character> list = new ArrayList<>();
-    static int[] score;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         N = Integer.parseInt(br.readLine());
-        arr =new String[N];
+        arr = new String[N];
+
+        Map<Character, Integer> hm = new HashMap<>();
 
         for (int i = 0; i < N; i++) {
             arr[i] = br.readLine();
-            for (int j = 0; j < arr[i].length(); j++) {
+            int pos = 1;
+            for (int j = arr[i].length() - 1; j >= 0; j--) {
                 char c = arr[i].charAt(j);
-                if (!list.contains(c)) list.add(c);
+                hm.put(c, hm.getOrDefault(c, 0) + pos);
+                pos *= 10;
             }
         }
 
-        score = new int[list.size()];
+        List<Character> keyList = new ArrayList<>(hm.keySet());
+        keyList.sort((o1, o2) -> Integer.compare(hm.get(o2), hm.get(o1)));
 
-        solve(0, 0);
+        int[] score = new int[26];
+        int value = 9;
+        for (int i = 0; i < keyList.size(); i++) {
+            score[keyList.get(i) - 'A'] = value--;
+        }
+
+        ans = 0;
+        for (int i = 0; i < N; i++) {
+            String cur = arr[i];
+            int num = 0;
+            for (int j = 0; j < cur.length(); j++) {
+                num = num * 10 + score[cur.charAt(j) - 'A'];
+            }
+            ans += num;
+        }
 
         bw.write(String.valueOf(ans));
         bw.flush();
         bw.close();
         br.close();
     }
-
-    private static void solve(int cnt, int flag) {
-        if (cnt==list.size()) {
-            ans = Math.max(ans,sum());
-            return;
-        }
-
-        for (int i = 0; i < 10; i++) {
-            if ((flag & (1 << i))!=0) continue;
-            score[cnt] = i;
-            solve(cnt + 1, flag | (1 << i));
-        }
-    }
-
-    private static int sum() {
-        int sum = 0;
-        for (int i = 0; i < N; i++) { // 문자열
-            String curr = arr[i];
-            int num = 0;
-            for (int j = 0; j<curr.length();j++){
-                char c = curr.charAt(j);
-                int charIdx = list.indexOf(c);
-                int point = score[charIdx];
-                num = num*10 + point;
-            }
-            sum+=num;
-        }
-
-        return sum;
-    }
-
 }
