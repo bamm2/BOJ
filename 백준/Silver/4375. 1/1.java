0912796c376cfd8f7089e6bdee93c02d986ec41b
bamm2 <96399409+bamm2@users.noreply.github.com>
2024-01-22 13:20:48 +0900

    [Silver III] Title: 1, Time: 72 ms, Memory: 11476 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,36 +1,24 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.math.BigInteger;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
 
         String input;
-        while ((input = br.readLine())!=null && !input.isEmpty()) {
+        while ((input = br.readLine())!=null) {
             int N = Integer.parseInt(input);
-            BigInteger comp = BigInteger.ONE;
-            BigInteger div = BigInteger.valueOf(N);
+            long comp = 1;
             int cnt = 1;
-            while (true) {
-                if (comp.remainder(div) == BigInteger.ZERO ) {
-                    sb.append(cnt).append('\n');
-                    break;
-                }
+            while (comp%N!=0) {
+                comp = (comp * 10 + 1)%N;
                 cnt++;
-                comp = comp.multiply(BigInteger.TEN).add(BigInteger.ONE);
             }
+            System.out.println(cnt);
         }
 
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
         br.close();
     }
 
