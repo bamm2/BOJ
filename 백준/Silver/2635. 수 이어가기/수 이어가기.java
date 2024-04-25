ee6763ba606a461aa75def51fd5c5d4631afe279
bamm2 <96399409+bamm2@users.noreply.github.com>
2024-04-25 10:48:00 +0900

    [Silver V] Title: 수 이어가기, Time: 112 ms, Memory: 16352 KB -BaekjoonHub

--- 백준/Silver/2635. 수 이어가기/수 이어가기.java
+++ 백준/Silver/2635. 수 이어가기/수 이어가기.java
@@ -1,60 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
+    static List<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb= new StringBuilder();
-        StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
 
-        int maxcnt=Integer.MIN_VALUE;
-        int chkidx=0;
-        // 0 ~ N까지 수 모두 체크해보기
-        for(int i=1;i<=N;i++){
-            int start=N;
-            int next=i;
-            int idx=i;
-            int cnt=1;
-            while(true){
-                int tmp=start-next;
-                start=next;
-                next=tmp;
-                cnt++;
-                if(start-next<0){
-                    cnt++;
-                    break;
-                }
-            }
-            if(cnt>maxcnt){
-                maxcnt=cnt;
-                chkidx=idx;
-            }
-        }
-        if(maxcnt==Integer.MIN_VALUE) sb.append(0);
-        else sb.append(maxcnt).append('\n');
-        int start=N;
-        int next=chkidx;
-        while(true){
-            if(start==0 && next==0) break;
-
-            sb.append(start+" ");
-            int tmp=start-next;
-            start=next;
-            next=tmp;
-            if(start-next<0){
-                sb.append(start+" "+next);
-                break;
-            }
+        for (int i = 0; i <= N; i++) {
+            search(N, i, new ArrayList<>());
         }
 
-        System.out.println(sb);
+        System.out.println(result());
+        br.close();
+    }
 
+    private static String result() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(list.size()).append('\n');
+        for (Integer elements : list) {
+            sb.append(elements).append(" ");
+        }
+        return sb.toString();
+    }
 
-
-    } // main
+    private static void search(int N, int num, ArrayList<Integer> candidates) {
+        if (N >= 0) candidates.add(N);
+        else {
+            if (list.size() < candidates.size()) {
+                list.clear();
+                list.addAll(candidates);
+            }
+            return;
+        }
+        search(num, N - num, candidates);
+    }
 }
