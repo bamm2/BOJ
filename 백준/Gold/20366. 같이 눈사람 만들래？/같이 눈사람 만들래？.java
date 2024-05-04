7dfa4508e206c0214e998f6ed7469460a73bb687
bamm2 <96399409+bamm2@users.noreply.github.com>
2024-05-04 10:39:49 +0900

    [Gold III] Title: 같이 눈사람 만들래?, Time: 584 ms, Memory: 40352 KB -BaekjoonHub

---
+++ 백준/Gold/20366. 같이 눈사람 만들래？/같이 눈사람 만들래？.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static class Point {
+        int idxOne, idxTwo;
+        long sum;
+
+        public Point(int idxOne, int idxTwo, long sum) {
+            this.idxOne = idxOne;
+            this.idxTwo = idxTwo;
+            this.sum = sum;
+        }
+
+        boolean contains(int compIdxOne, int compIdxTwo) {
+            return idxOne==compIdxOne || idxOne==compIdxTwo || idxTwo==compIdxOne || idxTwo==compIdxTwo;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        List<Point> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i==j) continue;
+                long sum = (long) arr[i] + arr[j];
+                list.add(new Point(i, j, sum));
+            }
+        }
+
+        list.sort((Comparator.comparingLong(o -> o.sum)));
+
+        int left = 0;
+        int right = 0;
+        long minDiff = Long.MAX_VALUE;
+        while (right < list.size()) {
+            Point L = list.get(left);
+            Point R = list.get(right);
+
+            if (left==right) right++;
+            else left++;
+
+            if (R.contains(L.idxOne, L.idxTwo)) continue;
+
+            minDiff = Math.min(R.sum - L.sum, minDiff);
+            if (minDiff==0L) break;
+        }
+
+        System.out.println(minDiff);
+        br.close();
+    }
+}
