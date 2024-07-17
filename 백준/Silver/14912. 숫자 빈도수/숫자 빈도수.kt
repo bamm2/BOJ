caa48ffc4f712fce5fe30730c47d809373f4f19c
bamm2 <96399409+bamm2@users.noreply.github.com>
2024-07-17 09:16:20 +0900

    [Silver V] Title: 숫자 빈도수, Time: 148 ms, Memory: 21160 KB -BaekjoonHub

--- 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.kt
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.kt
@@ -1,13 +1,13 @@
 fun main() {
-    val count = IntArray(10)
     val (n, d) = readln().split(" ").map { it.toInt() }
+    var result = 0
     for (i in 1..n) {
         var num = i
         while (true) {
-            count[num % 10]++
+            if(num%10 == d) result++
             num /= 10
             if (num == 0) break
         }
     }
-    println(count[d])
+    println(result)
 }
