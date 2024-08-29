3ce491b22bb0dbcb95221255cdf71ad8d1afffe9
bamm2 <96399409+bamm2@users.noreply.github.com>
2024-08-29 21:11:21 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 31.39 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.kt
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.kt
@@ -1,65 +1,68 @@
 class Solution {
-    
+
     private data class GiftInfo(
-        var give:Int = 0,
-        var take:Int = 0,
-        val info: HashMap<String,Int> = hashMapOf()
+        var give: Int = 0,
+        var take: Int = 0,
+        val info: HashMap<String, Int> = hashMapOf()
     )
-    private val infos = hashMapOf<String,GiftInfo>()
-    private val counter = hashMapOf<String,Int>()
-    
+
+    private val infos = hashMapOf<String, GiftInfo>()
+    private val counter = hashMapOf<String, Int>()
+
     fun solution(friends: Array<String>, gifts: Array<String>): Int {
-        friends.forEach{
+        friends.forEach {
             infos[it] = GiftInfo()
         }
-        
-        gifts.forEach{
-            val(from,to) = it.split(" ")
+
+        gifts.forEach {
+            val (from, to) = it.split(" ")
             infos[to]!!.take++
             val giveUserInfo = infos[from]!!
             giveUserInfo.give++
             val giveUserInfoMap = giveUserInfo.info
-            giveUserInfoMap.put(to,giveUserInfoMap.getOrDefault(to,0)+1)
+            giveUserInfoMap.customAdd(to)
         }
-        
-        for(i in 0.. friends.size-1){
+
+        for (i in 0..friends.size - 1) {
             val curFriends = friends[i]
-            for(j in i+1 .. friends.size-1){
+            for (j in i + 1..friends.size - 1) {
                 val compFriends = friends[j]
-                if(infos[curFriends]!!.info[compFriends] != null ||
-                        infos[compFriends]!!.info[curFriends] != null ){
-                    var cur = infos[curFriends]!!.info[compFriends] ?: 0
-                    var comp = infos[compFriends]!!.info[curFriends] ?: 0
-                    if(cur > comp){
-                        counter.put(curFriends,counter.getOrDefault(curFriends,0)+1)
-                    }else if(cur < comp){
-                        counter.put(compFriends,counter.getOrDefault(compFriends,0)+1)
-                    }else{
-                        cur = infos[curFriends]!!.give - infos[curFriends]!!.take
-                        comp = infos[compFriends]!!.give - infos[compFriends]!!.take
-                        if(cur > comp) {
-                              counter.put(curFriends,counter.getOrDefault(curFriends,0)+1)
-                        }else if(cur<comp){
-                              counter.put(compFriends,counter.getOrDefault(compFriends,0)+1)
-                        }
+                if (infos[curFriends]!!.info[compFriends] != null ||
+                    infos[compFriends]!!.info[curFriends] != null
+                ) {
+                    val cur = infos[curFriends]!!.info[compFriends] ?: 0
+                    val comp = infos[compFriends]!!.info[curFriends] ?: 0
+                    if (cur > comp) {
+                        counter.customAdd(curFriends)
+                    } else if (cur < comp) {
+                        counter.customAdd(compFriends)
+                    } else {
+                        findBiggerIncrease(curFriends, compFriends)
                     }
-                }else {
-                    val cur = infos[curFriends]!!.give - infos[curFriends]!!.take
-                    val comp = infos[compFriends]!!.give - infos[compFriends]!!.take
-                        if(cur > comp) {
-                              counter.put(curFriends,counter.getOrDefault(curFriends,0)+1)
-                        }else if(cur<comp){
-                              counter.put(compFriends,counter.getOrDefault(compFriends,0)+1)
-                        }
+                } else {
+                    findBiggerIncrease(curFriends, compFriends)
                 }
             }
         }
-        
-        var answer = 0
-        for(count in counter){
-            answer = Math.max(answer,count.value)
+       
+        var result = 0
+        for(user in counter){
+            result = Math.max(result,user.value)
         }
-        
-        return answer
+        return result
+    }
+
+    private fun HashMap<String, Int>.customAdd(name: String) {
+        this.put(name, this.getOrDefault(name, 0) + 1)
+    }
+
+    private fun findBiggerIncrease(curFriends: String, compFriends: String) {
+        val cur = infos[curFriends]!!.give - infos[curFriends]!!.take
+        val comp = infos[compFriends]!!.give - infos[compFriends]!!.take
+        if (cur > comp) {
+            counter.customAdd(curFriends)
+        } else if (cur < comp) {
+            counter.customAdd(compFriends)
+        }
     }
 }
