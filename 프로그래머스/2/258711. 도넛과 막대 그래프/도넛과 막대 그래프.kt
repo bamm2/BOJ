81525d7c86e7d1d9c3becfee2db5eb3de718c172
bamm2 <96399409+bamm2@users.noreply.github.com>
2024-10-15 14:13:28 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 1860.20 ms, Memory: 436 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.kt
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.kt
@@ -1,21 +1,23 @@
 class Solution {
+    private var MAX = 1_000_001L
     private lateinit var connects: MutableMap<Int, MutableList<Int>>
 
     fun solution(edges: Array<IntArray>): IntArray {
         connects = mutableMapOf()
-        val counter = IntArray(1_000_001)
-        var root = -1
+        val counter = mutableMapOf<Int, Int>()
         edges.forEach {
-            counter[it[0]]++
-            counter[it[1]]--
+            counter[it[0]] = counter.getOrDefault(it[0], 0) + 1
+            counter[it[1]] = counter.getOrDefault(it[1], 0) - 1
 
             val value = connects[it[0]] ?: mutableListOf()
             value.add(it[1])
             connects[it[0]] = value
         }
-        for (i in 0..1_000_000) {
-            if (counter[i] >= 2) {
-                root = i
+
+        var root = 0
+        for (element in counter) {
+            if (element.value >= 2) {
+                root = element.key
                 break
             }
         }
@@ -32,16 +34,14 @@ class Solution {
 
     private fun findShape(v: Int): Int {
         val q = ArrayDeque<Int>()
-        val dummy = 1_000_001L
+        val dummy = MAX
         val nodeCounter = hashSetOf(v)
         val edgeCounter = hashSetOf<Long>()
         q.addLast(v)
-        var flag = false
         while (q.isNotEmpty()) {
             val from = q.removeFirst()
             if (connects[from] == null) continue
             for (to in connects[from]!!) {
-                if (to == v) flag = true
                 if (edgeCounter.add(from * dummy + to)) {
                     q.addLast(to)
                     nodeCounter.add(to)
@@ -49,9 +49,9 @@ class Solution {
             }
         }
 
-        if (!flag) return 2
-
         return if (nodeCounter.size == edgeCounter.size) 1
-        else 3
+        else if (nodeCounter.size + 1 == edgeCounter.size) 3
+        else 2
     }
+
 }
